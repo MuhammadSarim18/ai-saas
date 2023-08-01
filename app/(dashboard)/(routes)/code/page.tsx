7967f9bbd2ee065272c8ee255886b4b9d328b21a
Muhammad Sarim <muhammadsarimofficial17@gmail.com>
2023-08-01 12:53:57 +0500

    code ai

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -2,10 +2,13 @@
 
 import axios from "axios";
 import * as z from "zod"
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { ChatCompletionRequestMessage } from "openai";
+import ReactMarkdown from "react-markdown";
 
 import { Heading } from "@/components/heading";
 
@@ -19,14 +22,12 @@ import { Button } from "@/components/ui/button";
 import { Empty } from "@/components/empty";
 
 import { formSchema } from "./constants";
-import { useState } from "react";
-import { ChatCompletionRequestMessage } from "openai";
 import { Loader } from "@/components/loader";
 import { cn } from "@/lib/utils";
 import { UserAvavtar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
 
-const ConversationPage = () => {
+const CodePage = () => {
     const router = useRouter();
     const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
 
@@ -48,7 +49,7 @@ const ConversationPage = () => {
 
             const newMessages = [...messages, userMessage];
 
-            const responce = await axios.post('/api/conversation', {
+            const responce = await axios.post('/api/code', {
                 messages: newMessages,
             })
             setMessages((current) => [...current, userMessage, responce.data])
@@ -63,11 +64,11 @@ const ConversationPage = () => {
     return (
         <div>
             <Heading
-                title="Conversation"
-                description="Our most advanced conversation model."
-                icon={MessageSquare}
-                iconColor="text-violet-500"
-                bgColor="bg-violet-500/10"
+                title="Code Generation"
+                description="Generate code using descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
@@ -84,7 +85,7 @@ const ConversationPage = () => {
                                                 className="border-0 outline-none focus-visible:ring-0
                                         focus-visible:ring-transparent"
                                                 disabled={isLoading}
-                                                placeholder="How do I calculate the radius of circle?"
+                                                placeholder="Simple toggle button using react hooks."
                                                 {...field}
                                             />
                                         </FormControl>
@@ -117,9 +118,21 @@ const ConversationPage = () => {
                                 )}
                             >
                                 {message.role === "user" ? <UserAvavtar /> : <BotAvatar />}
-                                <p className="text-sm">
-                                    {message.content}
-                                </p>
+                                <ReactMarkdown
+                                    components={{
+                                        pre: ({ node, ...props }) => (
+                                            <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                                <pre {...props} />
+                                            </div>
+                                        ),
+                                        code: ({ node, ...props }) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                    )
+                                    }}
+                                    className="text-sm overflow-hidden leading-7"
+                                >
+                                    {message.content || ""}
+                                </ReactMarkdown>
                             </div>
                         ))}
                     </div>
@@ -129,4 +142,4 @@ const ConversationPage = () => {
     );
 }
 
-export default ConversationPage;
+export default CodePage;
